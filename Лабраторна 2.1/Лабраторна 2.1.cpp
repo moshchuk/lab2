3f01d5c7b6753ba4b70cf811b453d583e6a4268b
Arbross <victor.pavlovskyi@gmail.com>
2024-09-13 18:09:11 +0200

    Commited code

---
+++ Лабраторна 2.1/Лабраторна 2.1.cpp
@@ -0,0 +1,32 @@
+﻿// Lab_02.1.cpp
+// < Мощук Мар'яна >
+// Лабораторна робота № 2.1
+// Лінійні програми.
+// Варіант 0.9
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_CTYPE, "ukr");
+
+    double alpha;
+    double betta;
+
+    cout << "Введiть значення alpha: " << endl;
+    cin >> alpha;
+
+    cout << "Введiть значення betta: " << endl;
+    cin >> betta;
+
+    /*double z1 = pow((cos(alpha) - cos(betta)), 2) - pow((sin(alpha) - sin(betta)), 2);
+    cout << "z1 = " << z1 << endl;
+
+    double z2 = -4 * pow(sin((alpha - betta) / 2), 2) * cos(alpha - betta);
+    cout << "z2 = " << z2 << endl;*/
+
+    return 0;
+}
